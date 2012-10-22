17e10928ee573ee542c0cef0eb582bdc958e5c80
Mats Kindahl <mats.kindahl@oracle.com>
2012-10-22 12:09:19 +0200

    HAM-40: Remove deprecated decorators
    This patch removes the decorators 'coordinated' and 'primitive' and the test
    for them since they are no longer needed after pushing HAM-30.

--- lib/tests/test_executor.py
+++ lib/tests/test_executor.py
@@ -11,7 +11,6 @@ from tests.utils import DummyManager
 
 count = []
 other = None
-inc = 0
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -23,34 +22,6 @@ def test1(job):
 def test2(job):
     global other
     other = 47
-
-@_executor.primitive
-def test1_primitive(job):
-    pass
-
-@_executor.primitive
-def test2_primitive(job):
-    raise Exception("Error...")
-
-@test2_primitive.undo
-def test2_primitive_undo(job):
-    global inc
-    inc += 1
-
-@_executor.primitive
-def test3_primitive(job):
-    raise Exception("Error...")
-
-@_executor.coordinated
-def test1_coordinated(job):
-    global inc
-    inc += 1
-
-@_executor.coordinated
-def test2_coordinated(job):
-    global inc
-    inc += 1
-    raise Exception("Error...")
 
 class Action(object):
     def __init__(self, expect):
@@ -102,76 +73,6 @@ class TestExecutor(unittest.TestCase):
                         _executor.Job.COMPLETE)
         self.assertTrue(job_2.status[-1]["description"] == \
                         "Executed action (test2).")
-
-        # Scheduling decorated actions to be executed.
-        job_primitive_1 = self.executor.enqueue_job(test1_primitive,
-                                                    "Enqueuing action test1()",
-                                                    True)
-        _LOGGER.debug("Job 1:"+ str(job_primitive_1))
-        job_primitive_2 = self.executor.enqueue_job(test2_primitive,
-                                                    "Enqueuing action test2()",
-                                                    True)
-        _LOGGER.debug("Job 2:"+ str(job_primitive_2))
-        job_primitive_3 = self.executor.enqueue_job(test3_primitive,
-                                                    "Enqueuing action test3()",
-                                                    True)
-        _LOGGER.debug("Job 3:"+ str(job_primitive_3))
-
-        # Check information on jobs
-        job = self.executor.get_job(job_primitive_1.uuid)
-        self.assertTrue(job_primitive_1.uuid == job.uuid)
-        self.assertTrue(job_primitive_1.status[-1]["success"] == \
-                        _executor.Job.SUCCESS)
-        self.assertTrue(job_primitive_1.status[-1]["state"] == \
-                        _executor.Job.COMPLETE)
-        self.assertTrue(job_primitive_1.status[-1]["description"] == \
-                        "Executed action (test1_primitive).")
-        job = self.executor.get_job(job_primitive_2.uuid)
-        self.assertTrue(job_primitive_2.uuid == job.uuid)
-        self.assertTrue(job_primitive_2.status[-1]["success"] == \
-                        _executor.Job.ERROR)
-        self.assertTrue(job_primitive_2.status[-1]["state"] == \
-                        _executor.Job.COMPLETE)
-        self.assertTrue(job_primitive_2.status[-1]["description"] == \
-                        "Tried to execute action (test2_primitive).")
-        job = self.executor.get_job(job_primitive_3.uuid)
-        self.assertTrue(job_primitive_3.uuid == job.uuid)
-        self.assertTrue(job_primitive_3.status[-1]["success"] == \
-                        _executor.Job.ERROR)
-        self.assertTrue(job_primitive_3.status[-1]["state"] == \
-                        _executor.Job.COMPLETE)
-        self.assertTrue(job_primitive_3.status[-1]["description"] == \
-                        "Tried to execute action (test3_primitive).")
-        self.assertEqual(inc, 1)
-
-        # Scheduling coordinated actions to be executed.
-        job_coordinated_1 = self.executor.enqueue_job(test1_coordinated,
-                                                      "Enqueuing action test1()",
-                                                      True)
-        _LOGGER.debug("Job 1:"+ str(job_coordinated_1))
-        job_coordinated_2 = self.executor.enqueue_job(test2_coordinated,
-                                                      "Enqueuing action test2()",
-                                                      True)
-        _LOGGER.debug("Job 2:"+ str(job_coordinated_2))
-
-        # Check information on jobs
-        job = self.executor.get_job(job_coordinated_1.uuid)
-        self.assertTrue(job_coordinated_1.uuid == job.uuid)
-        self.assertTrue(job_coordinated_1.status[-1]["success"] == \
-                        _executor.Job.SUCCESS)
-        self.assertTrue(job_coordinated_1.status[-1]["state"] == \
-                        _executor.Job.COMPLETE)
-        self.assertTrue(job_coordinated_1.status[-1]["description"] == \
-                        "Executed action (test1_coordinated).")
-        job = self.executor.get_job(job_coordinated_2.uuid)
-        self.assertTrue(job_coordinated_2.uuid == job.uuid)
-        self.assertTrue(job_coordinated_2.status[-1]["success"] == \
-                        _executor.Job.ERROR)
-        self.assertTrue(job_coordinated_2.status[-1]["state"] == \
-                        _executor.Job.COMPLETE)
-        self.assertTrue(job_coordinated_2.status[-1]["description"] == \
-                        "Tried to execute action (test2_coordinated).")
-        self.assertEqual(inc, 3)
 
         # Shutdown the executor and wait until its main thread returns.
         self.executor.shutdown()
