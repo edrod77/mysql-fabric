b94d5e435ca9e1701a2a4bf42f54226fe22377d2
Alfranio Correia <alfranio.correia@oracle.com>
2014-09-01 12:36:19 +0100

    BUG#18450008: MYSQLFABRIC IN UTILITIES 1.4.2 RETURNS A ATTRIBUTEERROR WHEN INVOKED
    This patch checks whether the connector python is installed and has the
    expected version.

--- lib/mysql/fabric/__init__.py
+++ lib/mysql/fabric/__init__.py
@@ -14,9 +14,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 #
-
 """MySQL Fabric library
 """
+import os
 
 # Version info as a tuple (major, minor, patch, extra)
 __version_info__ = (1, 4, 4, "")
@@ -24,3 +24,36 @@ __version_info__ = (1, 4, 4, "")
 # MySQL Fabric version:
 # `PEP-386 <http://www.python.org/dev/peps/pep-0386>`__ format
 __version__ = "{0}.{1}.{2}{3}".format(*__version_info__)
+
+# Connector Python version info as a tuple (major, minor, patch, extra)
+__cpy_version_info__ = (1, 2, 2, "")
+
+# Connector Python version:
+# `PEP-386 <http://www.python.org/dev/peps/pep-0386>`__ format
+__cpy_version__ = "{0}.{1}.{2}{3}".format(*__cpy_version_info__)
+
+def check_connector():
+    """Check if the connector is properly configured and
+    has the required version.
+    """
+    from mysql.fabric.errors import (
+        ConfigurationError
+    )
+
+    try:
+        import mysql.connector as cpy
+        if cpy.version.VERSION < __cpy_version_info__:
+            path = os.path.dirname(cpy.__file__)
+            raise ConfigurationError(
+                "Looked for mysql.connector at ({path}). Connector has "
+                "({cpy_ver}) version but ({required_ver}) or later version "
+                "is required.".format(path=path, cpy_ver=cpy.version.VERSION,
+                required_ver=__cpy_version_info__)
+            )
+    except ImportError as error:
+        import mysql
+        path = os.path.dirname(mysql.__file__)
+        raise ConfigurationError(
+            "Tried to look for mysql.connector at ({path}). Connector not "
+            "installed. Error ({error}).".format(path=path, error=error)
+        )
